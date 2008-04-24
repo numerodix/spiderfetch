d4f7f88c26d10236f5ecf879f0b11fd54854befc
Martin Matusiak <numerodix@gmail.com>
2008-04-24 16:24:25 +0200

    quote vars in bash script

--- asx_spiderfetch.sh
+++ asx_spiderfetch.sh
@@ -4,7 +4,7 @@ mypath=$(cd $(dirname $0); pwd)
 webpage="$1"
 
 for url in $($mypath/spiderfetch.rb $webpage "\.asx$" --dump); do 
-	video=$($mypath/spiderfetch.rb $url "^mms" --dump)
-	mplayer -dumpstream $video -dumpfile $(basename $video)
+	video=$($mypath/spiderfetch.rb "$url" "^mms" --dump)
+	mplayer -dumpstream "$video" -dumpfile $(basename "$video")
 done
 
