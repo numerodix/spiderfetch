27410c023844ecc15e78568d3322a74fb031225a
Martin Matusiak <numerodix@gmail.com>
2008-04-30 19:25:50 +0200

    dumpstream: quoting

--- dumpstream.sh
+++ dumpstream.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash 
 
 cat /dev/null > /tmp/getlog
 while read url; do 
-	echo "mplayer -dumpstream -dumpfile \"$(basename $url)\" \"$url\" 2>&1; "
-	mplayer -dumpstream -dumpfile "$(basename $url)" "$url" 2>&1; 
+	echo "mplayer -dumpstream -dumpfile '$(basename "$url")' '$url' 2>&1; "
+	mplayer -dumpstream -dumpfile "$(basename "$url")" "$url" 2>&1; 
 	e=$?
 	if [ $e != "0" ]; then 
 		echo "EXIT NONZERO:  $e   $url" >>/tmp/getlog ; 
