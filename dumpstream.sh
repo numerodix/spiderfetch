5eb8bc00e9298f27832fd238bac1295a5425fe2c
Martin Matusiak <numerodix@gmail.com>
2008-04-30 14:11:53 +0200

    handy dumpstream executer

---
+++ dumpstream.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat /dev/null > /tmp/getlog
+while read url; do 
+	echo "mplayer -dumpstream -dumpfile \"$(basename $url)\" \"$url\" 2>&1; "
+	mplayer -dumpstream -dumpfile "$(basename $url)" "$url" 2>&1; 
+	e=$?
+	if [ $e != "0" ]; then 
+		echo "EXIT NONZERO:  $e   $url" >>/tmp/getlog ; 
+	fi; 
+done
+
+echo "LOG:"
+cat /tmp/getlog
