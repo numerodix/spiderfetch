2519ef9d6bae766c9c784ce361fd844ca9119ee3
Martin Matusiak <numerodix@gmail.com>
2014-03-18 21:12:30 +0100

    bump version, update license

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     },
 
     classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
